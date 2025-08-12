b82bf6a25396fdddf289aaa06d3754e3cdd734c9
anxo <apineiroandion@danielcastelao.org>
2025-08-12 10:46:44 +0200

    a

--- RickAndMortyExplorer/Data/Remote/CharacterAPI.swift
+++ RickAndMortyExplorer/Data/Remote/CharacterAPI.swift
@@ -5,3 +5,18 @@
 //  Created by Anxo on 12/8/25.
 //
 
+import Foundation
+
+struct CharacterAPI {
+    func getCharacters() async throws -> [Character] {
+        let url = URL(string: "https://rickandmortyapi.com/api/character")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+
+        struct APIResponse: Decodable {
+            let results: [Character]
+        }
+
+        let decoded = try JSONDecoder().decode(APIResponse.self, from: data)
+        return decoded.results
+    }
+}
