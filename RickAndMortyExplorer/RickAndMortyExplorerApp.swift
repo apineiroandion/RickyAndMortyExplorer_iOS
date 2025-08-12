8f0f912b7a8d4a52a1b08badd62851bdfc54f309
anxo <apineiroandion@danielcastelao.org>
2025-08-12 10:26:36 +0200

    Initial Commit

---
+++ RickAndMortyExplorer/RickAndMortyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyExplorerApp.swift
+//  RickAndMortyExplorer
+//
+//  Created by Anxo on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyExplorerTests/RickAndMortyExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyExplorerTests.swift
+//  RickAndMortyExplorerTests
+//
+//  Created by Anxo on 12/8/25.
+//
+
+import Testing
+@testable import RickAndMortyExplorer
+
+struct RickAndMortyExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
